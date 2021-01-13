0dad6700fab035dfbfb14e0f4c92eb0689641622
Luna <Kobali17@yandex.ru>
2021-01-13 18:44:57 +0300

    route validation

--- routes/article.js
+++ routes/article.js
@@ -5,7 +5,18 @@ const {
 } = require('../controllers/article');
 
 router.get('/articles', getCards);
-router.post('/articles', createCard);
+router.post('/articles',celebrate({
+  body: Joi.object().keys({
+    keyword:Joi.string().required(),
+    title:Joi.string().required(),
+    text:Joi.string().required(),
+    date:Joi.string().required(),
+    source:Joi.string().required(),
+    image:Joi.string().required().uri(),
+    name: Joi.string().required(),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
 router.delete('/articles/articleId', deleteCard);
 
 module.exports = router;
